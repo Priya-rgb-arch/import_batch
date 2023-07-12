bb01fceff1fb94353b1335beee6268692efe6733
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-12 14:59:58 +0530

    Update import_data.sh

--- src/main/resources/import_data.sh
+++ src/main/resources/import_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Change to the directory where your Java program is located
-cd C:/Users/ARANKHAM/Downloads/ImportFDBFile/ImportFDBFile/FDBImporter
+ cd C:/Users/ARANKHAM/Downloads/ImportFDBFile/ImportFDBFile/FDBImporter
 
 # Set the Java classpath and the FDB file path
-CLASSPATH="C:/Users/ARANKHAM/Downloads/ImportFDBFile/ImportFDBFile/target/ImportFDBFile-0.0.1-SNAPSHOT.jar"
-FDB_FILE="C:/Users/ARANKHAM/OneDrive - Capgemini/Desktop/Batch_Jobs Files/FDB Daily price updates.csv"
+CLASSPATH="../target/ImportFDBFile-0.0.1-SNAPSHOT.jar"
+FDB_FILE="../resources/FDB Daily price updates.csv"
 
 # Compile and run the Java program to push changes to store database
 javac FDBImporter.java
