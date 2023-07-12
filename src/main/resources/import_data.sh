d8c7cd31220a2a31dd831302af4f798df604dc4a
Priya-rgb-arch <67141434+Priya-rgb-arch@users.noreply.github.com>
2023-07-12 15:13:33 +0530

    Update import_data.sh

--- src/main/resources/import_data.sh
+++ src/main/resources/import_data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Change to the directory where your Java program is located
- cd src/main/java/com/fdb/
+ #cd src/main/java/com/fdb/
 
 # Set the Java classpath and the FDB file path
 CLASSPATH="../target/ImportFDBFile-0.0.1-SNAPSHOT.jar"
 FDB_FILE="../resources/FDB Daily price updates.csv"
 
 # Compile and run the Java program to push changes to store database
-javac FDBImporter.java
-java FDBImporter
+javac src/main/java/com/fdb/FDBImporter.java
+java src/main/java/com/fdb/FDBImporter
